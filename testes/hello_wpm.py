65f6ff78ef4d0b7e41dccc56e5b68134dc71c884
Nilton Guedes Duarte <nilton.gduarte@gmail.com>
2016-07-22 02:15:15 -0300

    sugar!

--- testes/hello_wpm.py
+++ testes/hello_wpm.py
@@ -8,5 +8,7 @@ from wishful_module_wifi_wmp import *
 
 if __name__ == "__main__":
 	wmpm = WmpModule()
+	args = {'interface' : 'wlan0', UPI_R.CSMA_CW : 15, UPI_R.CSMA_CW_MIN : 15, UPI_R.CSMA_CW_MAX : 15}
+	result = wmpm.setParameterLowerLayer(args)
 	print(wmpm)
 	print ("hello wmp")
