dc6f4249c983797e0546216951487046dbd1f237
Nilton Guedes Duarte <nilton.gduarte@gmail.com>
2016-07-28 00:03:05 -0300

    executable python
    bash script, git chmod not working

---
+++ change_permission.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#file chaging file permission on git is not working
+#this should be made by:
+#git update-index --chmod=+x <file>
+
+chmod +x ./source/controller.py
+chmod +x ./source/agent.py 
+
+chmod +x ./testes/controller.py
+chmod +x ./testes/agent.py 
