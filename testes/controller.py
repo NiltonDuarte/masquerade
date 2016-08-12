a31949e6f5059d8404a43905fc90fc92c5973a23
Nilton Guedes Duarte <nilton.gduarte@gmail.com>
2016-08-12 02:16:20 -0300

    .

--- testes/controller.py
+++ testes/controller.py
@@ -98,9 +98,9 @@ def get_channel_reponse(group, node, data):
 def statCallback(group, node, data):
     print("{} AgentStatisticsCallback : Group:{}, NodeName:{}, msg:{}".format(datetime.datetime.now(), group, node.name, data))
     dataDict = ast.literal_eval(data)
-    dataSum = sum(dataDict.values())
-    cwPrio.updateValue(node, dataSum if dataSum > 0 else 1)
-    print("AgentStatisticsCallback sum:{}, squareQueue:{}".format(dataSum, dataDict['gatherTxQueueEMA']**2+dataDict['gatherTxBytesDEMA']))
+    dataVal = dataDict['gatherTxQueueEMA']#sum(dataDict.values())
+    cwPrio.updateValue(node, dataVal if dataVal > 0 else 1)
+    print("AgentStatisticsCallback sum:{}, squareQueue:{}".format(dataVal, dataDict['gatherTxQueueEMA']**2+dataDict['gatherTxBytesDEMA']))
 
 
 
