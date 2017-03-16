96ec59465342a12ca498de42744ff61ffb812456
NiltonDuarte <nilton.gduarte@gmail.com>
2017-03-16 17:10:41 -0300

    .

--- testes/controller.py
+++ testes/controller.py
@@ -120,7 +120,7 @@ try:
             for (node, cw) in nodeList:
                 cw = max(4,cw)
                 cw = min(256,cw)
-                print("Node :" + node + "CW: " + cw)
+                print("Node :" + node.name + "CW: " + cw)
                 controller.blocking(False).node(node).radio.iface("wlan0").set_parameter_lower_layer(interface='wlan0',
                     CSMA_CW_MIN=cw, CSMA_CW_MAX=cw*4)
             gevent.sleep(1)
